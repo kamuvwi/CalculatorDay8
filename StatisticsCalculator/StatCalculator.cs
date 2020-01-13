f9aaf69528fe53ade85d137a97c19e34402a95a8
Kamuvwi Lungu <kamuvwi.lungu@szi.gov.zm>
2020-01-13 16:28:56 +0200

    Feature: Added Z-Score operations, interfaces and tests, and added it

---
+++ StatisticsCalculator/StatCalculator.cs
@@ -0,0 +1,36 @@
+﻿using BasicCalculator;
+using StatMean;
+using StatStandardDev;
+using StatVariance;
+using StatZScore;
+
+namespace StatisticsCalculator
+{
+    public class StatCalculator : Calculator, IFindMean, IFindStandDev, IFindVariance, IFindZScore
+    {
+        private readonly FindMean mean = new FindMean();
+        private readonly FindStandDev standDev = new FindStandDev();
+        private readonly FindVariance var = new FindVariance();
+        private readonly FindZScore z = new FindZScore();
+
+        public dynamic Mean(dynamic values)
+        {
+            return mean.Mean(values);
+        }
+
+        public double Variance(dynamic values)
+        {
+            return var.Variance(values);
+        }
+
+        public double StandDev(dynamic values)
+        {
+            return standDev.StandDev(values);
+        }
+
+        public double ZScore(dynamic score, dynamic values)
+        {
+            return z.ZScore(score, values);
+        }
+    }
+}
